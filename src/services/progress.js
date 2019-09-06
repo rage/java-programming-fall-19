46141b4d8dc9cf8686da50719a0a8708a3d44c6b
Henrik Nygren <nygrenh@gmail.com>
2019-09-06 13:26:30 +0300

    Fix quizzes progress

--- src/services/progress.js
+++ src/services/progress.js
@@ -16,11 +16,10 @@ const introductionCourseGroups = [
 
 export async function fetchProgress() {
   // await fetchQuizzesProgress()
-  const serviceIdentifiers = ["Ohjelmointitehtävät", "Kyselyt", "Crowdsorcerer"]
+  const serviceIdentifiers = ["Programming exercises", "Quizzes"]
   const progressesCollection = await Promise.all([
     fetchProgrammingProgress(),
     fetchQuizzesProgress(),
-    fetchCrowdsorcererProgress(),
   ])
   const userDetails = await getCachedUserDetails()
   const currentCourseVariant = userDetails?.extra_fields?.course_variant
--- src/services/quizzes.js
+++ src/services/quizzes.js
@@ -3,7 +3,7 @@ import { accessToken } from "./moocfi"
 
 export async function fetchQuizzesProgress() {
   const response = await axios.get(
-    "https://quizzes.mooc.fi/api/v1/courses/38240a7b-7e64-4202-91e2-91f6d46f6198/users/current/progress",
+    "https://quizzes.mooc.fi/api/v1/courses/d4dbe426-33dd-41df-b934-0a4eaa5658fb/users/current/progress",
     { headers: { Authorization: `Bearer ${accessToken()}` } },
   )
   return response.data?.points_by_group
@@ -11,7 +11,7 @@ export async function fetchQuizzesProgress() {
 
 export async function fetchQuizNames() {
   const response = await axios.get(
-    "https://quizzes.mooc.fi/api/v1/quizzes/38240a7b-7e64-4202-91e2-91f6d46f6198/titles/fi_FI",
+    "https://quizzes.mooc.fi/api/v1/quizzes/d4dbe426-33dd-41df-b934-0a4eaa5658fb/titles/fi_FI",
   )
   return response.data
 }
